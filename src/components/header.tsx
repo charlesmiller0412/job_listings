f5f036a5664e6f26bec29d385b911a28d455d85f
Charles Miller <charles.miller.0412@gmail.com>
2023-02-01 12:15:27 -0600

    fix accessibility issues

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,13 @@ import bgMobile from "/images/bg-header-mobile.svg";
 
 export default function Header() {
     return (
-        <div className="header">
-            <div className="header__mobile">
-                <img src={bgMobile} />
-            </div>
-            <div className="header__desktop">
-                <img src={bgDesktop} />
-            </div>
-        </div>
+        <header className="header">
+            <picture className="header__mobile">
+                <img src={bgMobile} alt="" />
+            </picture>
+            <picture className="header__desktop">
+                <img src={bgDesktop} alt="" />
+            </picture>
+        </header>
     );
 }
